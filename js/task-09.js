5ae55144d8474df00a43d617e51346f6cc054540
Tetiana Ruzhetska <tetiana.ruzhetska@gmail.com>
2023-02-07 18:35:06 +0200

    Update task 9 (rgb to hex color)

--- js/task-09.js
+++ js/task-09.js
@@ -1,13 +1,20 @@
+const colorName = document.querySelector('.widget .color');
+const button = document.querySelector('.widget .change-color');
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-const colorName = document.querySelector('.widget .color');
-const button = document.querySelector('.widget .change-color');
-
 button.addEventListener('click', onChangeColor);
 
 function onChangeColor(event) {
-  document.body.style.backgroundColor = getRandomHexColor();
-  colorName.textContent = document.body.style.backgroundColor;
+  colorName.textContent = getRandomHexColor();
+  document.body.style.backgroundColor = colorName.textContent;
+
+
+
+  // document.body.style.backgroundColor = getRandomHexColor();
+  // colorName.textContent = document.body.style.backgroundColor;
+
 }
